b24a8c5c0751603879b2fb85f62535e5a444e726
skyle7602 <96888857+skyle7602@users.noreply.github.com>
2022-02-02 10:20:53 +0200

    Update hello.lua

--- hello.lua
+++ hello.lua
@@ -1,3 +1,4 @@
+local UiLib = tostring(math.random(100000,200000))..tostring(math.random(100000,200000))..tostring(math.random(100000,200000))
 for i,v in pairs(game.CoreGui:GetChildren()) do
 	if v.Name == "UiLib" then
 		v:Destroy()
